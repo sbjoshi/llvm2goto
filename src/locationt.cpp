639d7cedcd8ad6b26748d0341a970f19f4c905f6
rasika <cs16mtech11014@iith.ac.in>
2017-06-25 11:38:23 +0530

    added locationt.cpp

---
+++ src/locationt.cpp
@@ -0,0 +1,27 @@
+/* Copyright
+
+
+*/
+#include "locationt.h"
+
+
+
+source_locationt locationt::get_location_global_variable(
+    const MDNode *mdn) {
+    source_locationt location;
+    if (const DIVariable *digv = dyn_cast<DIVariable>(mdn)) {
+    location.set_file(digv->getFile()->getFilename().str());
+    location.set_working_directory(digv->getFile()->getDirectory().str());
+    location.set_line(digv->getLine());
+    }
+  return location;
+}
+
+// source_locationt locationt::get_location_local_variable(
+//     const llvm::DILocalVariable *dilv) {
+//     source_locationt location;
+//   location.set_file(dilv->getFile()->getFilename().str());
+//   location.set_working_directory(dilv->getFile()->getDirectory().str());
+//   location.set_line(dilv->getLine());
+//   return location;
+// }
