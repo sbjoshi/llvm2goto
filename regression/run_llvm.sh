06aa2b8987eaf1ad57bd125ed6e4ffef1ba70bb6
Akash Banerjee <TIFitis@github.com>
2019-01-23 17:09:57 +0530

    Support for cbmc-5.11

--- regression/run_llvm.sh
+++ regression/run_llvm.sh
@@ -5,6 +5,6 @@ do
    # echo "FILE : ${file}"
    # cp ${file} ${file}".i"	
    #sed -i 3's/$/ --bfs &/'  ${file}
-   /usr/local/bin/clang -S -g -emit-llvm ${file} -o ${file}".ll"
-   /home/akash/Documents/CBMC/cbmc/src/llvm2goto/llvm2goto ${file}".ll" -o ${file}".i"
+   <your_path_here>/clang -S -g -emit-llvm ${file} -o ${file}".ll"
+   /home/<your_path_here>/llvm2goto/llvm2goto ${file}".ll" -o ${file}".i"
 done
