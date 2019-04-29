cbfc75312cc700ab9d44904ea0b59a2d2cc6763e
Akash Banerjee <TIFitis@github.com>
2019-04-29 15:55:21 +0530

    Fixed the make files broken by last commit.

--- regression/run_llvm.sh
+++ regression/run_llvm.sh
@@ -2,9 +2,9 @@
 # A simple script
 for file in */*.c
 do
-   echo "FILE : ${file}"
+   # echo "FILE : ${file}"
    # cp ${file} ${file}".i"	
    #sed -i 3's/$/ --bfs &/'  ${file}
-   /home/akash/my_path/clang -S -g -emit-llvm -O0 ${file} -o ${file}".ll"
-   /home/akash/Documents/CBMC/cbmc/src/llvm2goto/llvm2goto ${file}".ll" -o ${file}".i"
+   <your_path_here>/clang -S -g -emit-llvm ${file} -o ${file}".ll"
+   /home/<your_path_here>/llvm2goto/llvm2goto ${file}".ll" -o ${file}".i"
 done
