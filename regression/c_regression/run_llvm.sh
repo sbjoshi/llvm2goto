b3accf0b7fcf4dd42f7a979d5f4876f22e127d22
TIFitis <cs18mtech11023@iith.ac.in>
2020-04-02 16:08:55 +0530

    Removed a test case taking too long to run, updated a compilation flag.

--- regression/c_regression/run_llvm.sh
+++ regression/c_regression/run_llvm.sh
@@ -2,10 +2,8 @@
 # A simple script
 for file in */*.c
 do
-   # echo "FILE : ${file}"
-   # cp ${file} ${file}".i"	
-   #sed -i 3's/$/ --bfs &/'  ${file}
-   rm ${file}".i"
-   clang -S -g -emit-llvm ${file} -o ${file}".ll" -Wno-implicit-function-declaration
-   ll2gb -o ${file}".i" ${file}".ll" -v
+   rm ${file}".i" -f
+   rm ${file}".ll" -f
+   clang -S -emit-llvm -Xclang -disable-O0-optnone ${file} -o ${file}".ll" -Wno-everything
+   ll2gb -o ${file}".i" ${file}".ll" -v 1 -opt
 done
