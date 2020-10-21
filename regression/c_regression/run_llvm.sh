bdd8d284c66b72598e7c0da84ae305f1ad342614
TIFitis <cs18mtech11023@iith.ac.in>
2020-10-21 20:00:15 +0530

    Updated CMAKE to better hanle variables

--- regression/c_regression/run_llvm.sh
+++ regression/c_regression/run_llvm.sh
@@ -5,5 +5,5 @@ do
    rm ${file}".i" -f
    rm ${file}".ll" -f
    clang -S -emit-llvm -Xclang -disable-O0-optnone ${file} -o ${file}".ll" -Wno-everything
-   ll2gb -o ${file}".i" ${file}".ll" -v 1 -opt
+   ../../build/ll2gb -o ${file}".i" ${file}".ll" -v 1 -opt
 done
